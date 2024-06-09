355306df0e2065a177b4921de8462525dc54a072
pablocla <pabloclavero03@gmail.com>
2024-06-09 13:24:52 -0300

    os

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     unoptimized: true,
   },
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Navbar = () => {
   return (
     <nav className="navbar bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="/">
-          <img src="/logo.png" alt="ResolbIT" className="logo" />
-        </a>
+        <Link href="/" legacyBehavior passHref>
+          <a>
+            <Image
+              src="/logo.png"
+              alt="ResolbIT"
+              width={50}
+              height={50}
+              className="logo"
+            />
+          </a>
+        </Link>
         <ul className="nav-links flex">
           <li>
-            <a href="/" className="px-4 py-2 hover:bg-gray-700 rounded">
-              Cotizar Software Personalizado
-            </a>
+            <Link href="/" legacyBehavior passHref>
+              <a className="px-4 py-2 hover:bg-gray-700 rounded">
+                Cotizar Software Personalizado
+              </a>
+            </Link>
           </li>
           <li>
-            <a href="/" className="px-4 py-2 hover:bg-gray-700 rounded">
-              E-commerce
-            </a>
+            <Link href="/ecommerce" legacyBehavior passHref>
+              <a className="px-4 py-2 hover:bg-gray-700 rounded">E-commerce</a>
+            </Link>
           </li>
           <li>
-            <a href="/" className="px-4 py-2 hover:bg-gray-700 rounded">
-              Cloud Services
-            </a>
+            <Link href="/cloud-services" legacyBehavior passHref>
+              <a className="px-4 py-2 hover:bg-gray-700 rounded">
+                Cloud Services
+              </a>
+            </Link>
           </li>
           <li>
-            <a href="/" className="px-4 py-2 hover:bg-gray-700 rounded">
-              Blockchain
-            </a>
+            <Link href="/blockchain" legacyBehavior passHref>
+              <a className="px-4 py-2 hover:bg-gray-700 rounded">Blockchain</a>
+            </Link>
           </li>
         </ul>
       </div>
